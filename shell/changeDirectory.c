a476ee5b0c4c5cb23658195b34b45e13b79cf1d5
kvillanueva <kvillanueva@miners.utep.edu>
2017-10-15 22:04:17 -0600

    Delete changeDirectory.c

--- shell/changeDirectory.c
+++
@@ -1,9 +0,0 @@
-/*Lab2: Shell-Part2
-Name: Kristian Villanueva
-Last Modification: 10/8/17
-*/
-#include <unistd.h>
-void changeDirectory(char**argv){
-	int chdir(const char argv[1]);
-	// printf("CD result : %d\n", result);
-}
